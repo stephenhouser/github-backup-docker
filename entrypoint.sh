21ad5019eec083230085f555af976053c3038560
Stephen Houser <stephenhouser@gmail.com>
2018-09-16 09:55:20 -0400

    include private repos

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 github-backup -u ${GITHUB_USER} -t ${GITHUB_TOKEN} \
-	--incremental --all --fork --bare --lfs --gists \
+	--incremental --private --gists --fork --all --bare --lfs \
 	-o /backup \
 	${GITHUB_BACKUP_USER}
